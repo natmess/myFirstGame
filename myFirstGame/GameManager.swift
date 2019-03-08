2cc22a15bf743aa53a9b16ebda444cd2f49b442a
natmess <nnmessier@gmail.com>
2019-03-08 18:16:42 -0500

    work in progress

---
+++ myFirstGame/GameManager.swift
@@ -0,0 +1,16 @@
+//
+//  GameManager.swift
+//  myFirstGame
+//
+//  Created by Nathalie  on 3/8/19.
+//  Copyright © 2019 Nathalie. All rights reserved.
+//
+
+import SpriteKit
+
+class GameManager {
+    var scene: GameScene!
+    init(scene: GameScene) {
+        self.scene = scene
+    }
+}
---
+++ myFirstGame/GameViewController.swift
@@ -0,0 +1,27 @@
+//
+//  GameViewController.swift
+//  myFirstGame
+//
+//  Created by Nathalie  on 2/6/19.
+//  Copyright © 2019 Nathalie. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+
+}
